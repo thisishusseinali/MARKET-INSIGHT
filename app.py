1a11909884c441ab4fb216e9f1878d83f691e88d
thisishusseinali <thisishusseinali@proton.me>
2023-11-24 09:15:59 +0200

    Add/Edit MARKET-INSIGHT 📈💡 Project Repo Code

---
+++ app.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+# ---- READ EXCEL ----
+@st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="./assets/supermarkt_sales.xlsx",
+        engine="openpyxl",
+        sheet_name="Sales",
+        skiprows=3,
+        usecols="B:R",
+        nrows=1000,
+    )
+    # Add 'hour' column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    df["Date"] = pd.to_datetime(df["Date"])
+    return df
+
+df = get_data_from_excel()
+
+# ---- SIDEBAR ----
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique(),
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+df_selection = df.query(
+    "City == @city & Customer_type ==@customer_type & Gender == @gender"
+)
+
+# Check if the dataframe is empty:
+if df_selection.empty:
+    st.warning("No data available based on the current filter settings!")
+    st.stop() # This will halt the app from further execution.
+
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
+
+# TOP KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
+
+# SALES BY PRODUCT LINE [BAR CHART]
+sales_by_product_line = df_selection.groupby(by=["Product line"])[["Total"]].sum().sort_values(by="Total")
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+    template="plotly_white",
+)
+fig_product_sales.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+# SALES BY HOUR [BAR CHART]
+sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Sales by hour</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+    template="plotly_white",
+)
+fig_hourly_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+
+# SALES BY CUSTOMER TYPE [PIE CHART]
+sales_by_customer_type = df_selection.groupby(by=["Customer_type"])[["Total"]].sum()
+fig_customer_type_sales = px.pie(
+    sales_by_customer_type,
+    values="Total",
+    names=sales_by_customer_type.index,
+    title="<b>Sales by Customer Type</b>",
+    template="plotly_white",
+)
+
+# TOTAL SALES TREND OVER TIME [LINE CHART]
+total_sales_trend = df_selection.groupby(by=df_selection["Date"].dt.date)["Total"].sum().reset_index()
+fig_sales_trend = px.line(
+    total_sales_trend,
+    x="Date",
+    y="Total",
+    title="<b>Total Sales Trend Over Time</b>",
+    template="plotly_white",
+)
+fig_sales_trend.update_xaxes(type='category')
+
+# Arrange charts in a 2x2 matrix
+col1, col2 = st.columns(2)
+
+with col1:
+    # Chart 1 - Sales by Product Line
+    st.plotly_chart(fig_product_sales, use_container_width=True)
+    # Chart 4 - Total Sales Trend Over Time
+    st.plotly_chart(fig_sales_trend, use_container_width=True)
+
+with col2:
+
+    # Chart 2 - Sales by Hour
+    st.plotly_chart(fig_hourly_sales, use_container_width=True)
+    # Chart 3 - Sales by Customer Type
+    st.plotly_chart(fig_customer_type_sales, use_container_width=True)
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
