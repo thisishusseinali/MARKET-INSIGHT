b4748d133995a46bcd0ceabaa83747b7bf20258d
Hussein Ali <111470519+thisishusseinali@users.noreply.github.com>
2023-11-24 09:28:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Market Insight - Supermarket Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 @st.cache_data
